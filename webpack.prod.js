5294de62e5b9123c65ed9c7094515806bdfdc689
Lukas Wöhrl <lukas@woehrl.net>
2022-04-07 19:12:05 +0000

    build: add gzip to build output

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,15 @@
 
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CompressionPlugin = require("compression-webpack-plugin")
 
 module.exports = merge(common, {
-
+  plugins: 
+  [  
+   new CompressionPlugin({    
+      test: /\.js(\?.*)?$/i,    
+      algorithm: "gzip",    
+      deleteOriginalAssets: false,  
+    }),
+  ],
 });
