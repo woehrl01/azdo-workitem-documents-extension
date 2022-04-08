31deaa89de6f4df9f616229e124917ab7f61808f
Lukas Wöhrl <lukas@woehrl.net>
2022-04-08 08:48:51 +0200

    fix: add FAQ section (#5)

--- README.md
+++ README.md
@@ -20,6 +20,20 @@ _If you have additional ideas, please create an [issue](https://github.com/woehr
 
 ![diagrams.net](static/diagrams.net.png)
 
+---
+
+### FAQ
+
+#### How can I hide the tab from specific work item types?
+
+You can hide the tab by [modifing your process](https://docs.microsoft.com/en-us/azure/devops/organizations/settings/work/customize-process-form?view=azure-devops). Use _Hide from layout_ on the specific work item type.
+
+![hide from layout](static/hide_from_layout.png)
+
+---
+
 In some scenarios, this extension might be treated as a must-have feature. I hope that it will be useful for you and your team. The extension is available on the [Azure DevOps Marketplace](https://marketplace.visualstudio.com/items?itemName=lukaswoehrl.azdo-workitem-documents) and on [GitHub](https://github.com/woehrl01/azdo-workitem-documents-extension) under MIT license!
+
+---
 
 _[Document icons created by Driss Lebbat - Flaticon](https://www.flaticon.com/free-icons/document)_
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,14 +2,35 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const CompressionPlugin = require("compression-webpack-plugin")
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(common, {
-  plugins: 
-  [  
-   new CompressionPlugin({    
-      test: /\.js(\?.*)?$/i,    
-      algorithm: "gzip",    
-      deleteOriginalAssets: false,  
-    }),
-  ],
+  plugins:
+    [
+      new CompressionPlugin({
+        test: /\.js(\?.*)?$/i,
+        algorithm: "gzip",
+        deleteOriginalAssets: false,
+      }),
+    ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            defaults: true,
+            pure_funcs: [
+              'console.log',
+              'console.debug',
+            ]
+          },
+          format: {
+            comments: false,
+          },
+        },
+        extractComments: false
+      })
+    ]
+  }
 });
