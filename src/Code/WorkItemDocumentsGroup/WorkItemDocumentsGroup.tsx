33dd067676367b56a0e7fd87c59bc61459787fb1
Lukas Wöhrl <lukas@woehrl.net>
2022-04-12 07:30:18 +0000

    match look and feel of 'Related Work'

--- src/Code/WorkItemDocumentsGroup/WorkItemDocumentsGroup.tsx
+++ src/Code/WorkItemDocumentsGroup/WorkItemDocumentsGroup.tsx
@@ -1,14 +1,48 @@
-import * as React from "react";
+import React from "react";
+import { useState, useEffect, useCallback } from "react";
+import * as SDK from "azure-devops-extension-sdk";
 import { FC } from "react";
 import { showRootComponent } from "../../Common";
-import { useLinkedDocuments } from "../../useLinkedDocument";
+import { ILinkedDocument, useLinkedDocuments } from "../../useLinkedDocument";
+import { Link } from "azure-devops-ui/Link"
+import "./WorkItemDocumentsGroup.scss";
+import { Icon } from "azure-devops-ui/Icon";
 
+const Document: FC<{ document: ILinkedDocument, additionalData?: string }> = ({ document, additionalData }) => {
+  return (
+    <div className="la-item">
+      <div className="la-item-wrapper">
+        <div className="la-artifact-data">
+          <div className="la-primary-data">
+            <Icon iconName="Attach" className="la-primary-icon" />
+            <Link href={document.url}>
+              {document.name}
+            </Link>
+          </div>
+          {
+            additionalData &&
+            <div className="la-additional-data">
+              <div className="la-additional-data-item">
+                <span className="la-text">{additionalData}</span>
+              </div>
+            </div>
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export const WorkItemFormGroupComponent: FC<{}> = ({ }) => {
   const documents = useLinkedDocuments();
-  return <ul>
-    {documents.map(d => <li key={d.url}>d.url</li>)}
-  </ul>
+
+  useEffect(() => {
+    SDK.resize()
+  }, [documents]);
+
+  return <>
+    {documents.map(d => <Document key={d.url} document={d} />)}
+  </>
 }
 
 showRootComponent(<WorkItemFormGroupComponent />);
