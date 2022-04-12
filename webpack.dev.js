0c114253c8eb2eb81d06cf31b80faa10a80e4fbf
Lukas Wöhrl <lukas@woehrl.net>
2022-04-12 22:41:55 +0200

    chore: refactoring and preparation for APM (#19)
    * feat: add apm for prod
    
    * disable apm

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,12 +3,12 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const path = require("path");
 
-module.exports = merge(common, {
+module.exports = merge(common("dev"), {
   devtool: "source-map",
   devServer: {
     static: {
       directory: path.join(__dirname, "dist"),
-      publicPath: "/dist",
+      publicPath: "dist/",
       watch: true,
     },
     watchFiles: ["src/**/*", "dist/**/*"],
