ba2927ffe2b193eacd0a491b9572adb16b0031b0
Lukas Wöhrl <lukas@woehrl.net>
2022-04-13 13:23:27 +0000

    add icons to tabs

--- src/Code/WorkItemDocumentsPage/Tabs.tsx
+++ src/Code/WorkItemDocumentsPage/Tabs.tsx
@@ -2,6 +2,21 @@ import { FC } from "react";
 import { Tab, TabBar, TabSize } from "azure-devops-ui/Tabs";
 import React from "react";
 import { ILinkedDocument } from "../../useLinkedDocument";
+import { getIcon } from "../../UriOptimizer";
+
+
+/* important: needs to be a function and not a (function-) component.
+ * otherwise the selection does not work */
+const createDocumentTab = (d: ILinkedDocument): JSX.Element => {
+    return <Tab
+        key={d.url}
+        className="document-tab"
+        name={d.name}
+        id={d.url}
+        iconProps={{ iconName: getIcon(d.url) }}
+    />;
+}
+
 
 export const Tabs: FC<{ documents: ILinkedDocument[]; onTabChanged: (id: string) => void; selectedTab: string; }> = ({ documents, onTabChanged, selectedTab }) => {
     if (documents.length <= 1) {
@@ -12,7 +27,8 @@ export const Tabs: FC<{ documents: ILinkedDocument[]; onTabChanged: (id: string)
         onSelectedTabChanged={onTabChanged}
         selectedTabId={selectedTab}
         tabSize={TabSize.Compact}>
-
-        {documents.map(t => <Tab key={t.url} className="document-tab" name={t.name} id={t.url} />)}
+        {documents.map(createDocumentTab)}
     </TabBar>;
 };
+
+
