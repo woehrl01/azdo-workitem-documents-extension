2c2a33b5bc29f3f6c095dd5d6143ef4b2f57da0f
Lukas Wöhrl <lukas@woehrl.net>
2022-04-13 15:25:24 +0200

    feat: dynamic icons and other ui improvements (#22)

--- src/Code/WorkItemDocumentsPage/Tabs.tsx
+++ src/Code/WorkItemDocumentsPage/Tabs.tsx
@@ -2,12 +2,19 @@ import { FC } from "react";
 import { Tab, TabBar, TabSize } from "azure-devops-ui/Tabs";
 import React from "react";
 import { ILinkedDocument } from "../../useLinkedDocument";
+import { getIcon } from "../../UriOptimizer";
 
-const stripName = (name: string): string => {
-    if (name.length > 25) {
-        return name.substring(0, 25) + "..."
-    }
-    return name;
+
+/* important: needs to be a function and not a (function-) component.
+ * otherwise the selection does not work */
+const createDocumentTab = (d: ILinkedDocument): JSX.Element => {
+    return <Tab
+        key={d.url}
+        className="document-tab"
+        name={d.name}
+        id={d.url}
+        iconProps={{ iconName: getIcon(d.url) }}
+    />;
 }
 
 
@@ -20,7 +27,8 @@ export const Tabs: FC<{ documents: ILinkedDocument[]; onTabChanged: (id: string)
         onSelectedTabChanged={onTabChanged}
         selectedTabId={selectedTab}
         tabSize={TabSize.Compact}>
-
-        {documents.map(t => <Tab key={t.url} name={stripName(t.name)} id={t.url} />)}
+        {documents.map(createDocumentTab)}
     </TabBar>;
 };
+
+
