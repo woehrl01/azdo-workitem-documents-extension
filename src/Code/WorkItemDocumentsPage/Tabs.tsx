75c09090c99ffb96f2c13d943cd01d818846c52e
Lukas Wöhrl <lukas@woehrl.net>
2022-04-16 23:57:43 +0000

    wip

--- src/Code/WorkItemDocumentsPage/Tabs.tsx
+++ src/Code/WorkItemDocumentsPage/Tabs.tsx
@@ -6,13 +6,15 @@ import styles from "./WorkItemDocumentsPage.module.scss";
 
 /* important: needs to be a function and not a (function-) component.
  * otherwise the selection does not work */
-const createDocumentTab = (d: ILinkedDocument): JSX.Element => <Tab
-    key={d.url}
-    className={styles.documentTab}
-    name={d.name}
-    id={d.url}
-    iconProps={{ iconName: getIcon(d.url) }}
-/>
+const createDocumentTab = (d: ILinkedDocument): JSX.Element => {
+    return <Tab
+        key={d.url}
+        className={styles.documentTab}
+        name={d.name}
+        id={d.url}
+        iconProps={{ iconName: getIcon(d.url) }}
+    />
+}
 
 
 export const Tabs: FC<{ documents: ILinkedDocument[]; onTabChanged: (id: string) => void; selectedTab: string; }> = ({ documents, onTabChanged, selectedTab }) => {
