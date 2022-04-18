b082ca9b9f50d5f251c9acb2d9c64043377ae1e0
Lukas Wöhrl <lukas@woehrl.net>
2022-04-18 18:14:55 +0000

    refactor styles

--- src/features/WorkItemDocumentsPage/PageContent/index.tsx
+++ src/features/WorkItemDocumentsPage/PageContent/index.tsx
@@ -1,7 +1,8 @@
 import { FC } from 'react';
 import { optimizeUrl } from 'services/UriOptimizer';
+import styles from './styles.module.scss';
 
 export const PageContent: FC<{ url: string; }> = ({ url }) => {
     const optimizedUrl = optimizeUrl(url);
-    return <iframe src={optimizedUrl} />;
+    return <iframe src={optimizedUrl} className={styles.frameContent} />;
 };
