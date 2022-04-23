a0a703663465b8209948d02a7a1580196f948e0c
Lukas Wöhrl <lukas@woehrl.net>
2022-04-23 18:10:01 +0200

    store value debounced

--- src/features/Settings/SettingsPage/component.tsx
+++ src/features/Settings/SettingsPage/component.tsx
@@ -7,6 +7,45 @@ import { TextField, TextFieldWidth } from 'azure-devops-ui/TextField';
 import styles from './style.module.scss';
 import { useStoredValue } from 'hooks/useStoredValue';
 import { Loading } from 'components/Loading';
+import { IReadonlyObservableValue, ObservableArray } from 'azure-devops-ui/Core/Observable';
+import { renderSimpleCell, Table, TableColumnLayout } from 'azure-devops-ui/Table';
+
+type ITableItem = {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const values = Array<ITableItem>({ name: 'Lukas', age: 50, gender: 'M' })
+
+const itemProvider = new ObservableArray<ITableItem | IReadonlyObservableValue<ITableItem | undefined>>(values);
+
+const asyncColumns = [
+  {
+    columnLayout: TableColumnLayout.singleLinePrefix,
+    id: 'name',
+    name: 'Name',
+    readonly: true,
+    renderCell: renderSimpleCell,
+    width: -30,
+  },
+  {
+    id: 'age',
+    name: 'Age',
+    readonly: true,
+    renderCell: renderSimpleCell,
+    width: -30
+  },
+  {
+    columnLayout: TableColumnLayout.none,
+    id: 'gender',
+    name: 'Gender',
+    readonly: true,
+    renderCell: renderSimpleCell,
+    width: -40,
+  },
+];
+
 
 export const SettingsPage: FC<NoProps> = () => {
   const { isLoading, value, setValue } = useStoredValue<string>('settings', '0');
@@ -25,6 +64,14 @@ export const SettingsPage: FC<NoProps> = () => {
           onChange={(_, newValue): void => setValue(newValue)}
           placeholder="Value"
           width={TextFieldWidth.standard}
+        />
+        <Table<ITableItem>
+          ariaLabel="Table with async"
+          className="table-example"
+          columns={asyncColumns}
+          containerClassName="h-scroll-auto"
+          itemProvider={itemProvider}
+          role="table"
         />
       </div>
     </Page>
--- src/hooks/useStoredValue/hook.ts
+++ src/hooks/useStoredValue/hook.ts
@@ -1,14 +1,13 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import * as SDK from 'azure-devops-extension-sdk';
 import { CommonServiceIds, IExtensionDataManager, IExtensionDataService } from 'azure-devops-extension-api';
-
+import { useDebounce } from 'usehooks-ts';
 
 export async function getDataManager(): Promise<IExtensionDataManager> {
   const service = await SDK.getService<IExtensionDataService>(CommonServiceIds.ExtensionDataService);
   const manager = await service.getExtensionDataManager(SDK.getContributionId(), await SDK.getAccessToken());
   return manager;
 }
-
 
 export type UseStoredValueState<T> = {
   value: T;
@@ -20,24 +19,36 @@ export const useStoredValue = <T>(name: string, defaultValue: T): UseStoredValue
   const [value, setValue] = useState<T>(defaultValue);
   const [loading, setLoading] = useState<boolean>(true);
 
+  /* load the data from data manager */
   useEffect(() => {
-    (async (): Promise<void> => {
+    let isLoadData = true;
+    const loadValue = async (): Promise<void> => {
       const manager = await getDataManager();
       const storedValue = await manager.getValue<T>(name);
-      if (storedValue) {
+      if (storedValue && isLoadData) {
         setValue(storedValue);
       }
       setLoading(false);
-    })();
+    };
+    loadValue().catch(console.error);
+    return (): void => { isLoadData = false }
   }, [name]);
 
-  const setStoredValue = useCallback((s: T): void => {
-    (async (): Promise<void> => {
-      setValue(s);
+  /* store the data in the datamanager in a debounced way */
+  const debouncedValue = useDebounce(value, 750);
+  useEffect(() => {
+    let isSetData = true;
+    const storeValue = async (): Promise<void> => {
       const manager = await getDataManager();
-      await manager.setValue(name, s);
-    })();
-  }, [setValue, name]);
+      if (isSetData) {
+        await manager.setValue(name, debouncedValue);
+      }
+    };
+    if (!loading) {
+      storeValue().catch(console.error);
+    }
+    return (): void => { isSetData = false };
+  }, [name, debouncedValue]);
 
-  return { isLoading: loading, value, setValue: setStoredValue };
+  return { isLoading: loading, value, setValue };
 };
