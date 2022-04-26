9b87ebc8e8d39bffc4e20fda2e77ed6bc6cb6322
Lukas Wöhrl <lukas@woehrl.net>
2022-04-26 05:07:51 +0000

    add comment

--- src/features/FormGroup/Group/component.tsx
+++ src/features/FormGroup/Group/component.tsx
@@ -4,16 +4,21 @@ import { AddButton } from '../AddButton';
 import * as SDK from 'azure-devops-extension-sdk';
 import { LinkedDocumentList } from '../LinkedDocumentList';
 
+const resizeHostFrame = (): void => {
+    SDK.resize()
+    console.debug('resize called')
+}
+
 export const Group = (): JSX.Element => {
+
+    /* we need to resize the group everytime the hosting frame is resized 
+     * This can be due to collapse/expand of the group or the browser windows
+     * size changes, etc. */
     const { width, height } = useWindowSize();
-    const resize = (): void => {
-        SDK.resize()
-        console.log('resize')
-    }
-    useEffect(resize, [width, height]);
+    useEffect(resizeHostFrame, [width, height]);
 
     return <div>
         <AddButton />
-        <LinkedDocumentList onDocuments={resize} />
+        <LinkedDocumentList onDocuments={resizeHostFrame} />
     </div>
 }
