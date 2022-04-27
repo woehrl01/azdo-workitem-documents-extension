3183f4100b77183d58848a7337199cb2f41dd3b6
Lukas Wöhrl <lukas@woehrl.net>
2022-04-27 20:34:48 +0000

    fix: set properties and measurements

--- src/components/AppInsights/index.tsx
+++ src/components/AppInsights/index.tsx
@@ -15,16 +15,17 @@ export const trackPageView = (): void => {
 }
 
 export class Measure {
+    private readonly startTime = Date.now();
     constructor(readonly name: string, readonly properties?: { [key: string]: string }) {
-        appInsights.startTrackEvent(name);
     }
 
     public stop(metrics?: { [key: string]: number }): void {
-        appInsights.stopTrackEvent(this.name, this.properties, metrics);
+        const duration = Date.now() - this.startTime;
+        trackEvent(this.name, { ...metrics, ...this.properties, duration });
     }
 }
 
-export const trackEvent = (name: string, properties?: { [key: string]: string }): void => {
+export const trackEvent = (name: string, properties?: { [key: string]: unknown }): void => {
     appInsights.trackEvent({ name }, properties);
 }
 
