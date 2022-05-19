c238c49f1c51fb41c4074189855bac451b2dfc20
Lukas Wöhrl <lukas@woehrl.net>
2022-05-19 22:07:32 +0000

    fix: remove unneeded dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import legacy from '@vitejs/plugin-legacy'
-import removeConsole from 'vite-plugin-remove-console';
 
 import { fileURLToPath } from 'url'
 import * as path from 'path'
@@ -18,7 +17,6 @@ export default defineConfig(({ command, mode }) => {
   return {
     plugins: [
       react(),
-      removeConsole(),
       legacy({
         targets: ['defaults', 'not IE 11']
       })
@@ -27,6 +25,9 @@ export default defineConfig(({ command, mode }) => {
     define: {
       __DEV__: mode === 'development',
       __APP_INSIGHTS__: JSON.stringify(env.APP_INSIGHTS_KEY),
+    },
+    esbuild: {
+      drop: ['console', 'debugger'],
     },
     resolve: {
       alias: {
