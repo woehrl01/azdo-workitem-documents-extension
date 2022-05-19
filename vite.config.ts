f3db817aaa13bc11e56c0901b096ca710497d3b9
Lukas Wöhrl <lukas@woehrl.net>
2022-05-19 21:58:59 +0000

    fix: remove console from production

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import legacy from '@vitejs/plugin-legacy'
+import removeConsole from 'vite-plugin-remove-console';
 
 import { fileURLToPath } from 'url'
 import * as path from 'path'
@@ -12,14 +13,12 @@ if (fs.existsSync('/certs/localhost.key')) {
   enableHttps = true
 }
 
-// https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-
   const env = loadEnv(mode, process.cwd(), '')
-
   return {
     plugins: [
       react(),
+      removeConsole(),
       legacy({
         targets: ['defaults', 'not IE 11']
       })
