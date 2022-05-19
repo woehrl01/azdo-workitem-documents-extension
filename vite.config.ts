e765877c4bbcf97ccacf0daea2e5da7f74497111
Lukas Wöhrl <lukas@woehrl.net>
2022-05-19 23:16:10 +0000

    build: add analyzer to build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import legacy from '@vitejs/plugin-legacy'
+import { visualizer } from 'rollup-plugin-visualizer';
 
 import { fileURLToPath } from 'url'
 import * as path from 'path'
@@ -17,6 +18,7 @@ export default defineConfig(({ command, mode }) => {
   return {
     plugins: [
       react(),
+      visualizer(),
       legacy({
         targets: ['defaults', 'not IE 11']
       })
