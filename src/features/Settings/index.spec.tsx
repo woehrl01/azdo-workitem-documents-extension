4719a1010fedabbd1ad7cb35708f0f24177ab499
Lukas Wöhrl <lukas@woehrl.net>
2022-05-20 13:30:50 +0200

    fix: optimize tests

--- src/features/Settings/index.spec.tsx
+++ src/features/Settings/index.spec.tsx
@@ -1,13 +1,12 @@
 import { describe, expect, it, vi } from 'vitest'
 import { SettingsPage } from './SettingsPage'
-import { flushPromises, render, screen, userEvent } from 'test/utils'
+import { render, screen, userEvent } from 'test/utils'
 
 vi.mock('azure-devops-extension-sdk')
 
 describe('Simple working test', () => {
     it('the title is visible', async () => {
         render(<SettingsPage />)
-        await flushPromises();
         const heading = await screen.findByRole('heading')
         expect(heading).toBeInTheDocument()
         expect(heading.textContent).toBe('Embedded Documents')
@@ -15,14 +14,9 @@ describe('Simple working test', () => {
 
     it('Should add new row when clicking the button', async () => {
         render(<SettingsPage />)
-        await flushPromises();
-
-        expect(await screen.queryAllByPlaceholderText(/rule/i)).toHaveLength(0)
+        expect(await screen.findAllByPlaceholderText(/rule/i)).toHaveLength(0)
 
         userEvent.click(screen.getByRole('menuitem', { name: /add rule/i }))
-
-        await flushPromises();
-
-        expect(await screen.queryAllByPlaceholderText(/rule/i)).toHaveLength(1)
+        expect(await screen.findAllByPlaceholderText(/rule/i)).toHaveLength(1)
     })
 })
