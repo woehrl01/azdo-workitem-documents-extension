2ddaafa8c3a03c5bd0ff494a096fe60699e901d1
Lukas Wöhrl <lukas@woehrl.net>
2022-05-22 19:28:21 +0000

    fix: preload services to be ready before first use

--- src/services/DataManager/index.ts
+++ src/services/DataManager/index.ts
@@ -2,7 +2,15 @@ import * as SDK from 'azure-devops-extension-sdk';
 import { IExtensionDataManager, IExtensionDataService } from 'azure-devops-extension-api';
 import { CommonServiceIds } from 'components/Common';
 
+
+const dataServicePromise = SDK.ready()
+  .then(() =>
+    Promise.all([
+      SDK.getService<IExtensionDataService>(CommonServiceIds.ExtensionDataService),
+      SDK.getAccessToken()
+    ])
+      .then(([dataService, token]) => dataService.getExtensionDataManager(SDK.getContributionId(), token)));
+
 export async function getDataManager(): Promise<IExtensionDataManager> {
-  const service = await SDK.getService<IExtensionDataService>(CommonServiceIds.ExtensionDataService);
-  return await service.getExtensionDataManager(SDK.getContributionId(), await SDK.getAccessToken());
+  return await dataServicePromise;
 }
--- src/services/OpenDialog/index.ts
+++ src/services/OpenDialog/index.ts
@@ -2,8 +2,10 @@ import * as SDK from 'azure-devops-extension-sdk';
 import { IHostPageLayoutService } from 'azure-devops-extension-api';
 import { CommonServiceIds } from 'components/Common';
 
+const dialogServicePromise = SDK.ready().then(() => SDK.getService<IHostPageLayoutService>(CommonServiceIds.HostPageLayoutService));
+
 export async function openAddDocumentDialog(): Promise<void> {
-    const dialogService = await SDK.getService<IHostPageLayoutService>(CommonServiceIds.HostPageLayoutService);
+    const dialogService = await dialogServicePromise;
 
     const options = {
         title: 'Add Document',
