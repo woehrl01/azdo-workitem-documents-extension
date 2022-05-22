fd984d9b550d658b88138c4c312e45590a92cfd6
Lukas Wöhrl <lukas@woehrl.net>
2022-05-22 19:30:21 +0000

    add comments

--- src/services/DataManager/index.ts
+++ src/services/DataManager/index.ts
@@ -2,7 +2,10 @@ import * as SDK from 'azure-devops-extension-sdk';
 import { IExtensionDataManager, IExtensionDataService } from 'azure-devops-extension-api';
 import { CommonServiceIds } from 'components/Common';
 
-
+/*
+we preload the data service as soon as the SDK is ready, so we we don't 
+have to wait for it when the user interacts with the extension.
+*/
 const dataServicePromise = SDK.ready()
   .then(() =>
     Promise.all([
